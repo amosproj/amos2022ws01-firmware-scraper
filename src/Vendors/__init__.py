93acee7e848ac660f2befc045b16b38b631da631
lpagel <lucy.viola.pagel@iav.de>
2022-11-14 18:02:05 +0100

    Add basic core structure and requirements.txt

---
+++ src/Vendors/__init__.py
@@ -0,0 +1,1 @@
+from AVM import AVMScraper
---
+++ src/core.py
@@ -0,0 +1,36 @@
+"""
+Core module for firmware scraper
+"""
+
+# Standard Libraries
+
+# Vendor Modules
+from Vendors import AVMScraper
+
+class Core:
+
+    def __init__(self,
+                vendor_list: list[str]):
+        self.vendor_list = vendor_list
+
+    def get_product_catalog(self):
+        pass
+
+    def compare_products(self):
+        pass
+
+    def download_firmware(self):
+        pass
+
+
+
+if __name__ == '__main__':
+
+    # Import config file
+    import json
+
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+
+
+    
