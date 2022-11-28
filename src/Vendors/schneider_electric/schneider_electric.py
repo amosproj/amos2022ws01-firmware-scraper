cb5574b9e5964813ef9b66bb0e8a23e46d49a0d0
Leon Schäffer <l.schaeffer@campus.tu-berlin.de>
2022-11-28 23:12:22 +0100

    Change manufacturer field value to "SchneiderElectric" for better compatibility with db_connector
    Signed-off-by: Leon Schäffer <l.schaeffer@campus.tu-berlin.de>

--- src/Vendors/schneider_electric/schneider_electric.py
+++ src/Vendors/schneider_electric/schneider_electric.py
@@ -23,9 +23,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from src.Vendors.scraper import Scraper
 from src.logger import create_logger
 
-DOWNLOAD_URL_GLOBAL = "https://www.se.com/ww/en/download/doc-group-type/3541958-Software%20&%20Firmware/?docType=1555893-Firmware"
-DOWNLOAD_URL_GERMANY = "https://www.se.com/de/de/download/doc-group-type/7090926-Software%20und%20Firmware/?docType=1555893-Firmware"
-DOWNLOAD_URL_USA = "https://www.se.com/us/en/download/doc-group-type/3587139-Software%20&%20Firmware/?docType=1555893-Firmware"
+DOWNLOAD_URL_GLOBAL = (
+    "https://www.se.com/ww/en/download/doc-group-type/3541958-Software%20&%20Firmware/?docType=1555893-Firmware"
+)
+DOWNLOAD_URL_GERMANY = (
+    "https://www.se.com/de/de/download/doc-group-type/7090926-Software%20und%20Firmware/?docType=1555893-Firmware"
+)
+DOWNLOAD_URL_USA = (
+    "https://www.se.com/us/en/download/doc-group-type/3587139-Software%20&%20Firmware/?docType=1555893-Firmware"
+)
 
 
 class SchneiderElectricScraper(Scraper):
@@ -37,16 +43,12 @@ class SchneiderElectricScraper(Scraper):
         self.scrape_entry_url = scrape_entry_url
         self.max_products = max_products
 
-        self.driver = webdriver.Chrome(
-            service=ChromeService(ChromeDriverManager().install())
-        )
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.implicitly_wait(0.5)  # has to be set only once
 
         self.logger = create_logger("Schneider Electric")
 
-    def _find_element_and_check(
-        self, product_page: WebElement, by: By, value: str
-    ) -> Optional[WebElement]:
+    def _find_element_and_check(self, product_page: WebElement, by: By, value: str) -> Optional[WebElement]:
         # we use find_elements instead of find_element to be aware if the CSS selector is able to locate a unique element
         elements = product_page.find_elements(by=by, value=value)
 
@@ -65,9 +67,7 @@ class SchneiderElectricScraper(Scraper):
         # here, we accept multiple found elements
         urls = [
             el.get_attribute("href")
-            for el in product_page.find_elements(
-                by=By.CSS_SELECTOR, value=CSS_SELECTOR_DOWNLOAD_URL
-            )
+            for el in product_page.find_elements(by=By.CSS_SELECTOR, value=CSS_SELECTOR_DOWNLOAD_URL)
         ]
 
         non_pdf_urls = []
@@ -81,15 +81,9 @@ class SchneiderElectricScraper(Scraper):
 
     def _extract_info(self, product_url: str) -> list[dict]:
         CSS_SELECTOR_TITLE = ".doc-title"
-        CSS_SELECTOR_RELEASE_DATE = (
-            ".doc-details-desktop > div:nth-child(1) > span:nth-child(1)"
-        )
-        CSS_SELECTOR_LANGUAGES = (
-            ".doc-details-desktop > div:nth-child(2) > span:nth-child(1)"
-        )
-        CSS_SELECTOR_VERSION = (
-            ".doc-details-desktop > div:nth-child(2) > span:nth-child(2)"
-        )
+        CSS_SELECTOR_RELEASE_DATE = ".doc-details-desktop > div:nth-child(1) > span:nth-child(1)"
+        CSS_SELECTOR_LANGUAGES = ".doc-details-desktop > div:nth-child(2) > span:nth-child(1)"
+        CSS_SELECTOR_VERSION = ".doc-details-desktop > div:nth-child(2) > span:nth-child(2)"
         CSS_SELECTOR_REFERENCE = "div.col-md-12:nth-child(3) > span:nth-child(1)"
         CSS_SELECTOR_PRODUCT_RANGES = ".range-block > .inner-1"
 
@@ -98,38 +92,24 @@ class SchneiderElectricScraper(Scraper):
         self.driver.get(product_url)
         product_page = self.driver.find_element(by=By.TAG_NAME, value="html")
 
-        if el := self._find_element_and_check(
-            product_page, By.CSS_SELECTOR, CSS_SELECTOR_TITLE
-        ):
+        if el := self._find_element_and_check(product_page, By.CSS_SELECTOR, CSS_SELECTOR_TITLE):
             # Some product titles are accompanied by an information stroke element. If it exists, it corrupts the
             # extracted title. Therefore, it is removed (if existent).
             title = el.text.removesuffix("/n information_stroke")
-        if el := self._find_element_and_check(
-            product_page, By.CSS_SELECTOR, CSS_SELECTOR_RELEASE_DATE
-        ):
+        if el := self._find_element_and_check(product_page, By.CSS_SELECTOR, CSS_SELECTOR_RELEASE_DATE):
             release_date_raw = el.text.removeprefix("Date : ").split("/")
-            release_date = (
-                f"{release_date_raw[2]}-{release_date_raw[1]}-{release_date_raw[0]}"
-            )
-        if el := self._find_element_and_check(
-            product_page, By.CSS_SELECTOR, CSS_SELECTOR_LANGUAGES
-        ):
+            release_date = f"{release_date_raw[2]}-{release_date_raw[1]}-{release_date_raw[0]}"
+        if el := self._find_element_and_check(product_page, By.CSS_SELECTOR, CSS_SELECTOR_LANGUAGES):
             languages = el.text.removeprefix("Languages : ")
-        if el := self._find_element_and_check(
-            product_page, By.CSS_SELECTOR, CSS_SELECTOR_VERSION
-        ):
+        if el := self._find_element_and_check(product_page, By.CSS_SELECTOR, CSS_SELECTOR_VERSION):
             version = el.text.removeprefix("Latest Version : ")
-        if el := self._find_element_and_check(
-            product_page, By.CSS_SELECTOR, CSS_SELECTOR_REFERENCE
-        ):
+        if el := self._find_element_and_check(product_page, By.CSS_SELECTOR, CSS_SELECTOR_REFERENCE):
             reference = el.text.removeprefix("Reference : ")
-        if el := self._find_element_and_check(
-            product_page, By.CSS_SELECTOR, CSS_SELECTOR_PRODUCT_RANGES
-        ):
+        if el := self._find_element_and_check(product_page, By.CSS_SELECTOR, CSS_SELECTOR_PRODUCT_RANGES):
             product_ranges = el.text.removeprefix("Product Ranges: ")
 
         firmware_item = {
-            "manufacturer": "Schneider Electric",
+            "manufacturer": "SchneiderElectric",
             "product_name": title,
             "product_type": product_ranges,
             "version": version,
@@ -151,34 +131,26 @@ class SchneiderElectricScraper(Scraper):
             self.logger.info(f"Scraped product '{title}'.")
             return [firmware_item]
         elif len(download_links) > 1:
-            self.logger.debug(
-                f"Found multiple download links for product '{title}' with URL '{product_url}'."
-            )
+            self.logger.debug(f"Found multiple download links for product '{title}' with URL '{product_url}'.")
             # When multiple (non-PDF) download links for a single product are found, a separate metadata dict for every
             # link is returned
             firmware_item_list = []
             for link in download_links:
                 firmware_item_copy = firmware_item.copy()
                 firmware_item_copy["download_link"] = link
-                firmware_item_copy["additional_data"][
-                    "other_download_links"
-                ] = download_links
+                firmware_item_copy["additional_data"]["other_download_links"] = download_links
                 firmware_item_list.append(firmware_item_copy)
             self.logger.info(f"Scraped product '{title}'.")
             return firmware_item_list
 
     def scrape_metadata(self) -> list[dict]:
         self.driver.get(self.scrape_entry_url)
-        self.logger.info(
-            f"Successfully accessed entry point URL {self.scrape_entry_url}."
-        )
+        self.logger.info(f"Successfully accessed entry point URL {self.scrape_entry_url}.")
 
         # on a single page, only a certain number (e.g. 10) of firmware products is displayed
         # pages must be iterated to retrieve all elements
         no_pages = int(
-            self.driver.find_element(by=By.CLASS_NAME, value="pager")
-            .find_element(by=By.CLASS_NAME, value="last")
-            .text
+            self.driver.find_element(by=By.CLASS_NAME, value="pager").find_element(by=By.CLASS_NAME, value="last").text
         )
 
         # iterate over result pages
@@ -186,12 +158,11 @@ class SchneiderElectricScraper(Scraper):
         for page in range(1, no_pages + 1):
             self.driver.get(f"{self.scrape_entry_url}&pageNumber={page}")
 
-            firmware_products = self.driver.find_element(
-                by=By.CLASS_NAME, value="result-list"
-            ).find_elements(by=By.CLASS_NAME, value="result-list-item")
+            firmware_products = self.driver.find_element(by=By.CLASS_NAME, value="result-list").find_elements(
+                by=By.CLASS_NAME, value="result-list-item"
+            )
             firmware_product_urls += [
-                item.find_element(by=By.CLASS_NAME, value="title").get_attribute("href")
-                for item in firmware_products
+                item.find_element(by=By.CLASS_NAME, value="title").get_attribute("href") for item in firmware_products
             ]
             if len(firmware_product_urls) > self.max_products:
                 break
@@ -208,9 +179,7 @@ class SchneiderElectricScraper(Scraper):
             if firmware_items := self._extract_info(product_url):
                 extracted_data += firmware_items
 
-        self.logger.info(
-            f"Finished scraping metadata of firmware products. Return metadata to core."
-        )
+        self.logger.info(f"Finished scraping metadata of firmware products. Return metadata to core.")
         self.driver.quit()
         return extracted_data
 
