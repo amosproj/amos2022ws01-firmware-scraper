3c156d156dcea39a52b0e4bc168524792ddd2e0b
oliver99str <oliver.strauch@campus.tu-berlin.de>
2022-11-29 17:06:26 +0100

    Updated Vendor, Scrapes multiple Paths
    Signed-off-by: oliver99str <oliver.strauch@campus.tu-berlin.de>

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from scraper import Zyxel_Scraper
 from zipfile import ZipFile
-import requests
 
 import pathlib as pl
 import os
 import keyboard
-import time
+#import time
 import io
 import PyPDF2
 
@@ -13,48 +12,49 @@ import PyPDF2
 VENDOR_URL = "https://www.zyxel.com/de/de/support/download"
 DRIVER_PATH = "C:\\Users\\Admin\\Desktop\\amos\\chromedriver_win32\\chromedriver.exe"
 
-def http_head(url:str):
-    res = requests.head(url)
-    print(res.status_code)
-    print(res.headers)
+# def http_head(url:str):
+#     res = requests.head(url)
+#     print(res.status_code)
+#     print(res.headers)
     
-def get_files(folder):
-    import os
-    os.chdir(folder)
-    files = os.listdir()
-    files = [x for x in files if x.endswith(".pdf")]
-    return files 
+# def get_files(folder):
+#     import os
+#     os.chdir(folder)
+#     files = os.listdir()
+#     files = [x for x in files if x.endswith(".pdf")]
+#     return files 
 
-def create_txt_files(path, filename):
-    KILL_KEY = 'esc'
-    tmp = path + "\\" + filename
-    with open(tmp, "rb") as pdf_file:
-        read_pdf = PyPDF2.PdfFileReader(pdf_file)
-        number_of_pages = read_pdf.getNumPages()
-        print(number_of_pages)
-        for i in range(number_of_pages):
-            time.sleep(1)
-            page = read_pdf.pages[i]
-            page_content = page.extractText()
-            print(page_content)
+# def create_txt_files(path, filename):
+#     KILL_KEY = 'esc'
+#     tmp = path + "\\" + filename
+#     with open(tmp, "rb") as pdf_file:
+#         read_pdf = PyPDF2.PdfFileReader(pdf_file)
+#         number_of_pages = read_pdf.getNumPages()
+#         print(number_of_pages)
+#         for i in range(number_of_pages):
+#             time.sleep(5)
+#             page = read_pdf.pages[i]
+#             page_content = page.extractText()
+#             print(page_content)
         
-    return
+#     return
     
-def file_unzip():
-    with ZipFile("C:\\Users\\Admin\\Desktop\\amos\\git\\amos2022ws01-firmware-scraper\\src\\GS1350-12HP_4.70(ABPJ.5)C0.zip", 'r') as zObject:
-        zObject.extractall(
-        path="C:\\Users\\Admin\\Desktop\\amos\\git\\amos2022ws01-firmware-scraper\\src\\temp")
+# def file_unzip():
+#     with ZipFile("C:\\Users\\Admin\\Desktop\\amos\\git\\amos2022ws01-firmware-scraper\\src\\GS1350-12HP_4.70(ABPJ.5)C0.zip", 'r') as zObject:
+#         zObject.extractall(
+#         path="C:\\Users\\Admin\\Desktop\\amos\\git\\amos2022ws01-firmware-scraper\\src\\temp")
         
-    files = get_files("C:\\Users\\Admin\\Desktop\\amos\\git\\amos2022ws01-firmware-scraper\\src\\temp")    
+#     files = get_files("C:\\Users\\Admin\\Desktop\\amos\\git\\amos2022ws01-firmware-scraper\\src\\temp")    
 
-    for i in files:
-        create_txt_files("C:\\Users\\Admin\\Desktop\\amos\\git\\amos2022ws01-firmware-scraper\\src\\temp", i)    
+#     for i in files:
+#         create_txt_files("C:\\Users\\Admin\\Desktop\\amos\\git\\amos2022ws01-firmware-scraper\\src\\temp", i)    
 
 def main():
-    Scraper = Zyxel_Scraper(VENDOR_URL, DRIVER_PATH)
-    download_links = Scraper.get_download_links()
-    [http_head(i) for i in download_links]
-    file_unzip()
+    Scraper = Zyxel_Scraper(DRIVER_PATH)
+    Scraper.get_product_catalog()
+    #download_links = Scraper.get_download_links()
+    #[http_head(i) for i in download_links]
+    #file_unzip()
     #print(Scraper.get_download_links())
 
 if __name__ == "__main__":
--- src/scraper.py
+++ src/scraper.py
@@ -1,17 +1,127 @@
+from unicodedata import category
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+import requests
+
+HOME_URL = "https://www.zyxel.com/global/en"
+PRODUCT_PATH = "/global/en/products"
+MANUFACTURER = "Zyxel Networks"
+DOWNLOAD_URL = "https://www.zyxel.com/global/en/support/download"
 
 class Zyxel_Scraper:
-    def __init__(self, vendor_url: str, driver_path: str):
-        self.vendor_url = vendor_url
+    def __init__(self, driver_path: str):
+        self.vendor_url = HOME_URL
         self.driver = webdriver.Chrome(driver_path)
 
-    def get_download_links(self):
+    def get_product_category_ulrs(self):
+        category_urls = []
+        
+        menu = self.driver.find_element(By.ID, "block-product-category-mega-menu")
+        wrapper = menu.find_elements(By.CLASS_NAME, "product-category-mega-menu-item")
+        
+        for category in wrapper:
+            links = category.find_elements(By.TAG_NAME, "a")    
+            cat = category.find_element(By.TAG_NAME, "h3")
+            #print(cat.get_attribute("innerHTML"))       
+            for link in links:
+                if type(link.get_attribute("href")) == str:
+                    if PRODUCT_PATH in link.get_attribute("href"):
+                        category_urls.append(link.get_attribute("href"))
+                        print(cat.get_attribute("innerHTML")+ link.get_attribute("href"))
+            
+        return category_urls
+        
+    # firmware_item = {"manufacturer": "Schneider Electric",
+    #                      "product_name": title,
+    #                      "product_type": product_ranges,
+    #                      "version": version,
+    #                      "release_date": None,
+    #                      "checksum_scraped": None,
+    #                      "additional_data": {
+    #                          "product_reference": reference,
+    #                          "languages": languages
+    #                      }
+    #                     }
+        
+    def get_products(self, category_urls: dict):
+        products = []
+        for category_url in category_urls:
+            #print(category_url)
+            self.driver.get(category_url)
+            
+            cat_name = self.driver.find_element(By.CLASS_NAME, "category-name")
+            x = cat_name.find_element(By.CLASS_NAME, "field")
+            product_type = x.get_attribute("innerHTML")
+            print("The category is: " + product_type)
+            
+            
+            products_of_category = self.driver.find_elements(By.CLASS_NAME, "product-item-info")
+            
+            for product in products_of_category:
+                product_name = (product.find_element(By.TAG_NAME, "h5")).get_attribute("innerHTML")
+                #print(product_name)
+                
+                is_in_list = False
+                for item in products:
+                    if item["product_name"] == product_name:
+                        is_in_list = True
+                
+                if not is_in_list:    
+                    firmware_item = {"manufacturer": MANUFACTURER,
+                            "product_name": product_name,
+                            "product_type": product_type,
+                            "version": None,
+                            "release_date": None,
+                            "checksum_scraped": None,
+                            "additional_data": {
+                                "product_reference": None,
+                                "languages": None
+                            }
+                            }
+                    products.append(firmware_item)
+                #print(firmware_item)
+                
+            time.sleep(0.3)
+
+        [print(x) for x in products]
+        return products
+        
+    def get_download_links(self, products: list):
         return_list = []
+        
+        #print(return_list)  
+
+        for product in products:
+            self.driver.get(DOWNLOAD_URL)
+            element = self.driver.find_element(By.NAME, "model")
+            send_button = self.driver.find_element(By.ID, "edit-submit-product-list-by-model")
+            pname = product["product_name"]
+            element.send_keys(pname)
+            send_button.click()
+            links = self.driver.find_elements(By.TAG_NAME, "a")
+            for link in links:
+                if type(link.get_attribute("href")) == str:
+                    if ".zip" in link.get_attribute("href"):
+                        return_list.append(link.get_attribute("href"))
+                        
+        for x in return_list:
+            res = requests.head(x)
+            print(res.headers)
+            
+        time.sleep(0.4)
+        
+
+    def get_product_catalog(self):
         self.driver.get(self.vendor_url)
-        links = self.driver.find_elements(By.TAG_NAME, "a")
-        for link in links:
-            if type(link.get_attribute("href")) == str:
-                if ".zip" in link.get_attribute("href"):
-                    return_list.append(link.get_attribute("href"))  
-        return return_list
+        category_urls = self.get_product_category_ulrs()
+        #print(category_urls)
+        products = self.get_products(category_urls)
+        self.get_download_links(products)
+        #cat = self.driver.find_element(By.ID, "block-product-category-mega-menu")
+        #cat_html = cat.get_attribute("innerHTML")
+        #print(cat_html)
+        
+        
+        
