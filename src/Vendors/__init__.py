5e82a48ea2bff54fb01b20f061391d40cd9d8698
kirilzilla <kirilm@me.com>
2022-12-01 15:41:12 +0700

    added innit

--- src/Vendors/__init__.py
+++ src/Vendors/__init__.py
@@ -2,8 +2,6 @@
 Scraping modules need to be imported in this file to be accessible in core.py
 """
 
-from synology.synology import Synology_scraper
-
 from .AVM.AVM import AVMScraper
 from .schneider_electric.schneider_electric import SchneiderElectricScraper
 from .synology.synology import Synology_scraper
