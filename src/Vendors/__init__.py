106eb49223b3a2e882596a45415a9c2448e30376
kirilzilla <kirilm@me.com>
2022-12-07 17:27:33 +0700

    changed name synology_scraper to SynologyScraper, sorry didnt know python likes CamelCase

--- src/Vendors/__init__.py
+++ src/Vendors/__init__.py
@@ -5,4 +5,4 @@ Scraping modules need to be imported in this file to be accessible in core.py
 from .AVM.AVM import AVMScraper
 from .schneider_electric.schneider_electric import SchneiderElectricScraper
 from .scraper import Scraper
-from .synology.synology import Synology_scraper
+from .synology.synology import SynologyScraper
