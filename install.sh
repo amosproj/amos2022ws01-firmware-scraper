f966e876952c9c39a1af293a842a8f8664ce0ea5
Lucy <lucy.v.pagel@gmail.com>
2023-01-29 17:16:31 +0100

    Add environment checks to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-echo "Checking system requirements"
+## Color definition
+export RED="\033[0;31m"
+export GREEN="\033[0;32m"
+export ORANGE="\033[0;33m"
+export MAGENTA="\033[0;35m"
+export CYAN="\033[0;36m"
+export BLUE="\033[0;34m"
+export NC="\033[0m"  # no color
+
+## Attribute definition
+export BOLD="\033[1m"
+
+echo -e "\\n""$GREEN""$BOLD""AMOS Firmware Scraper""$NC"
+
+echo -e "$NC""Checking system requirements..."
 
 SUB="Ubuntu 22.04"
 DISTRO=""
@@ -15,14 +29,46 @@ then
         fi
     fi
 else
-    echo "Warning: This program is only optimized for use under Ubuntu 22.04!"
+    echo -e "$RED""$BOLD""WARNING: This program is only optimized for use under Ubuntu 22.04!\n"
+    read -p "Do you want to continue the installation anyway? (y/n) " yn
+    case $yn in 
+        [yY] ) echo ;;
+        [nN] ) echo "exiting...";
+            exit;;
+        * ) echo "invalid response";;
+    esac
 fi
 
-echo "Setting up virtual environment"
+# Check if git exists and install if not
+echo -e "$NC""Installing necessary packages..."
+if ! command -v git > /dev/null ; then
+apt-get install -y git
+fi
+
+# Check if python exists and install if not
+if ! command -v python3.10 > /dev/null ; then
+apt-get install -y python3.10
+fi
+if ! command -v python-is-python3 > /dev/null ; then
+apt-get install -y python-is-python3
+fi
+if ! command -v pip3.10 > /dev/null ; then
+apt-get install -y python3-pip
+fi
+if ! command -v python3.10-venv > /dev/null ; then
+apt-get install -y python3.10-venv
+fi
+
+# Clone git repository
+echo -e "$NC""Cloning code repository..."
+git clone https://github.com/amosproj/amos2022ws01-firmware-scraper.git
+cd amos2022ws01-firmware-scraper
+
+echo -e "$NC""Setting up virtual environment..."
 python -m venv .venv
 source .venv/bin/activate
 
-echo "Installing dependencies..."
+echo -e "$NC""Installing dependencies..."
 pip install -r src/requirements.txt
 
-echo "Setup complete!"
+echo -e "$NC""Setup complete!"
