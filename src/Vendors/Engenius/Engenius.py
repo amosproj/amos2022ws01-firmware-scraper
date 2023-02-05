956e231636b6fdcf29acfce088371d2e4aae263d
oliver99str <oliver.strauch@campus.tu-berlin.de>
2023-02-05 11:30:05 +0100

    Engenius: Add Common Logger Messages

--- src/Vendors/Engenius/Engenius.py
+++ src/Vendors/Engenius/Engenius.py
@@ -3,20 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.common.exceptions import ElementNotInteractableException
-from selenium.common.exceptions import ElementClickInterceptedException
-
 from src.logger import *
 from src.Vendors.scraper import Scraper
 
 HOME_URL = "https://www.engeniusnetworks.eu/downloads/"
 MANUFACTURER = "Engenius"
-ignored_exceptions = (NoSuchElementException,
-                      StaleElementReferenceException,
-                      ElementNotInteractableException,
-                      ElementClickInterceptedException)
+ignored_exceptions = (Exception)
 
 
 class EngeniusScraper(Scraper):
@@ -41,7 +33,7 @@ class EngeniusScraper(Scraper):
         try:
             sel_accept_elem = self.driver.find_element(By.ID, SEL_COOKIE_ID)
             sel_accept_elem.click()
-            self.logger.info('Accepted Cookies')
+            self.logger.debug('Accepted Cookies')
         except ignored_exceptions:
             self.logger.error('Failed to Accept Cookies')
 
@@ -49,11 +41,12 @@ class EngeniusScraper(Scraper):
         SEL_FIRMWARE_OPTION_XPATH = '//option[@value="firmware"]'
 
         try:
-            sel_selector_elem = self.driver\
-                .find_element(By.XPATH, SEL_FIRMWARE_OPTION_XPATH)
-            sel_selector_elem.click()
+            sel_selector_elem = self.driver.find_element(
+                By.XPATH,
+                SEL_FIRMWARE_OPTION_XPATH
+            )
 
-            self.logger.info('Selected File Type -> Firmware')
+            sel_selector_elem.click()
         except ignored_exceptions:
             self.logger.error('Could not select File Type -> Firmware')
 
@@ -64,14 +57,20 @@ class EngeniusScraper(Scraper):
         try:
             sel_dl_menu = self.driver.find_element(By.ID, SEL_MENU_ID)
 
-            sel_category_class = sel_dl_menu\
-                .find_element(By.CLASS_NAME, SEL_CATEGORY_CLASS)
+            sel_category_class = sel_dl_menu.find_element(
+                By.CLASS_NAME,
+                SEL_CATEGORY_CLASS
+            )
 
-            sel_category_elements = sel_category_class\
-                .find_elements(By.XPATH, "./*")
+            sel_category_elements = sel_category_class.find_elements(
+                By.XPATH,
+                "./*"
+            )
 
-            self.logger.info("Categorys Found -> " +
-                             str(len(sel_category_elements)))
+            self.logger.debug(
+                "Categorys Found -> " +
+                str(len(sel_category_elements))
+            )
 
         except ignored_exceptions:
             self.logger.error("Could not find any Categorys")
@@ -87,19 +86,33 @@ class EngeniusScraper(Scraper):
         product_metadata = []
 
         try:
-            sel_dl_table_elem = self.driver\
-                .find_element(By.ID, SEL_DL_TABLE_ID)
+            sel_dl_table_elem = self.driver.find_element(
+                By.ID,
+                SEL_DL_TABLE_ID
+            )
 
-            sel_table_body_elem = sel_dl_table_elem\
-                .find_element(By.TAG_NAME, SEL_TABLE_BODY_TAG)
+            sel_table_body_elem = sel_dl_table_elem.find_element(
+                By.TAG_NAME,
+                SEL_TABLE_BODY_TAG
+            )
 
-            sel_file_elements = sel_table_body_elem\
-                .find_elements(By.TAG_NAME, 'tr')
+            sel_file_elements = sel_table_body_elem.find_elements(
+                By.TAG_NAME,
+                'tr'
+            )
+        except ignored_exceptions:
+            self.logger.warning(
+                'Could not find Download Table'
+            )
+            return []
 
-            for sel_file_elem in sel_file_elements:
-                sel_file_content_elements = sel_file_elem\
-                    .find_elements(By.TAG_NAME, 'td')
-
+        for sel_file_elem in sel_file_elements:
+            try:
+                sel_file_content_elements = sel_file_elem.find_elements(
+                    By.TAG_NAME,
+                    'td'
+                )
+                time.sleep(1)
                 if (sel_file_content_elements[0]
                         .get_attribute('innerHTML') == 'undefined'):
                     continue
@@ -115,8 +128,9 @@ class EngeniusScraper(Scraper):
                     "additional_data": {},
                 }
 
-                filename = sel_file_content_elements[0]\
-                    .get_attribute('innerHTML')
+                # filename = sel_file_content_elements[0].get_attribute(
+                #    'innerHTML'
+                # )
 
                 firmware_item["product_name"] = product_name
                 firmware_item["product_type"] = category_name
@@ -124,24 +138,33 @@ class EngeniusScraper(Scraper):
                 firmware_item["version"] = sel_file_content_elements[1]\
                     .get_attribute('innerHTML')
 
+                time.sleep(1)
+
                 firmware_item["release_date"] = sel_file_content_elements[4]\
                     .get_attribute('innerHTML')
 
+                time.sleep(1)
+
                 sel_link_elem = sel_file_content_elements[5]\
                     .find_element(By.TAG_NAME, 'a')
+
+                time.sleep(1)
 
                 firmware_item["download_link"] = sel_link_elem\
                     .get_attribute('href')
 
                 product_metadata.append(firmware_item)
 
-                self.logger.info('Firmware found -> (Category)' +
-                                 category_name + ' -> (Product)' +
-                                 product_name + ' -> (File Name)' +
-                                 filename)
-        except ignored_exceptions:
-            self.logger.error(
-                'Could not Scrape Product Metadata from Download Table')
+                self.logger.info(
+                    firmware_scraping_success(
+                        f"{firmware_item['product_name']} {firmware_item['download_link']}"
+                    )
+                )
+            except ignored_exceptions:
+                self.logger.warning(
+                    'Could not Scrape Product Metadata from Download Table'
+                )
+                continue
 
         return product_metadata
 
@@ -158,25 +181,36 @@ class EngeniusScraper(Scraper):
                 expected_conditions.element_to_be_clickable(category_element))\
                 .click()
         except ignored_exceptions:
-            self.logger.error(
-                'Failed to select Category')
+            self.logger.warning(
+                'Failed to select Category'
+            )
             return []
 
-        sel_category_name = category_element\
-            .find_element(By.CLASS_NAME, SEL_CATEGORY_NAME_CLASS)
+        sel_category_name = category_element.find_element(
+            By.CLASS_NAME,
+            SEL_CATEGORY_NAME_CLASS
+        )
 
         category_name = sel_category_name.get_attribute('innerHTML')
 
-        self.logger.info('Start Scraping Category -> ' + category_name)
-        sel_product_list_elem = category_element\
-            .find_element(By.CLASS_NAME, SEL_CHILD_LIST_CLASS)
+        self.logger.debug('Start Scraping Category -> ' + category_name)
+        sel_product_list_elem = category_element.find_element(
+            By.CLASS_NAME,
+            SEL_CHILD_LIST_CLASS
+        )
 
         time.sleep(10)
-        sel_product_elements = sel_product_list_elem\
-            .find_elements(By.XPATH, './*')
+        sel_product_elements = sel_product_list_elem.find_elements(
+            By.XPATH,
+            './*'
+        )
 
-        self.logger.info('Number of Products in ' + category_name +
-                         ' -> ' + str(len(sel_product_elements)))
+        self.logger.debug(
+            'Number of Products in '
+            + category_name
+            + ' -> '
+            + str(len(sel_product_elements))
+        )
 
         for sel_product_elem in sel_product_elements:
             try:
@@ -184,15 +218,18 @@ class EngeniusScraper(Scraper):
                     expected_conditions.element_to_be_clickable(
                         sel_product_elem)).click()
             except ignored_exceptions:
-                self.logger.error(
-                    'Failed to Click to select Product in Category -> ' +
-                    category_name)
+                self.logger.warning(
+                    'Failed to Click to select Product in Category -> '
+                    + category_name
+                )
                 continue
 
             time.sleep(2)
             product_name = sel_product_elem.get_attribute('innerHTML')
             product_metadata = self._scrape_product_metadata(
-                product_name, category_name)
+                product_name,
+                category_name
+            )
 
             category_metadata = category_metadata + product_metadata
 
@@ -208,14 +245,19 @@ class EngeniusScraper(Scraper):
         meta_data = []
         self.__scrape_cnt = 0
 
+        self.logger.important(start_scraping())
+        self.logger.debug('Headless -> ' + str(self.headless))
+
+        self.logger.debug(
+            'Max Products to Scrape -> '
+            + str(self.max_products)
+        )
+
         try:
             self.driver.get(self.scrape_entry_url)
-            self.logger.info(
-                "Successfully accessed entry point URL " +
-                self.scrape_entry_url)
-        except ignored_exceptions as e:
-            self.logger.error(
-                "Abort scraping. Could not access entry point URL" + e)
+            self.logger.important(firmware_url_success(self.scrape_entry_url))
+        except ignored_exceptions:
+            self.logger.error(firmware_scraping_failure(self.scrape_entry_url))
             self.driver.quit()
             return []
 
@@ -234,9 +276,8 @@ class EngeniusScraper(Scraper):
             if self.__scrape_cnt == self.max_products:
                 break
 
-        self.logger.info('Done Scraping Engenius Firmware')
-        self.logger.info('Total Engenius Firmware Scraped -> ' +
-                         str(len(meta_data)))
+        self.logger.debug('Metadata Found -> ' + str(len(meta_data)))
+        self.logger.important(finish_scraping())
 
         self.__scrape_cnt = 0
         self.driver.quit()
@@ -245,7 +286,21 @@ class EngeniusScraper(Scraper):
 
 
 if __name__ == "__main__":
-    Scraper = EngeniusScraper(get_logger(), max_products=5)
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    options = Options()
+    # options.add_argument("--headless")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--start-maximized")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), options=options
+    )
+    Scraper = EngeniusScraper(headless=False, max_products=50, driver=driver)
     metadata = Scraper.scrape_metadata()
     with open("scraped_metadata/firmware_data_Engenius.json", "w") as firmware_file:
         json.dump(metadata, firmware_file)
