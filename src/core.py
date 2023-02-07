6e2ab3f819ada9d4047a3770671396ad6103956d
Lucy <52314876+scherzgefaess@users.noreply.github.com>
2023-02-07 15:39:01 +0100

    Add Selenium Logging options to core

--- src/core.py
+++ src/core.py
@@ -28,6 +28,7 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--start-maximized")
 options.add_argument("--window-size=1920,1080")
+options.add_argument("log-level=3")
 
 
 class Core:
